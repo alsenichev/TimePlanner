a1c8f8bab16d86d29867f733d3d133f33fe5fe53
Aleksey Senichev <aleks.senichev@gmail.com>
2022-03-28 15:10:12 +0300

    migration for NextTime

---
+++ backend/TimePlanner.DataAccess/Migrations/20220328115852_WorkItemNextTime.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimePlanner.DataAccess.Migrations
+{
+    public partial class WorkItemNextTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecurrenceDays",
+                table: "WorkItems");
+
+            migrationBuilder.DropColumn(
+                name: "WakingUpWhere",
+                table: "WorkItems");
+
+            migrationBuilder.RenameColumn(
+                name: "WakingUpWhen",
+                table: "WorkItems",
+                newName: "NextTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NextTime",
+                table: "WorkItems",
+                newName: "WakingUpWhen");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecurrenceDays",
+                table: "WorkItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WakingUpWhere",
+                table: "WorkItems",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- backend/database/migrations.cmd
+++ backend/database/migrations.cmd
@@ -1,5 +1,9 @@
 dotnet ef migrations add InitialCreate -s ..\TimePlanner.WebApi\TimePlanner.WebApi.csproj
-
 dotnet ef database update -s ..\TimePlanner.WebApi\TimePlanner.WebApi.csproj
-
 dotnet ef database update -s ..\TimePlanner.WebApi\TimePlanner.WebApi.csproj --connection Server=ALEKSEY-HPZB\SQLEXPRESS;Database=TimePlanner;Trusted_Connection=True;MultipleActiveResultSets=true
+or
+Add-Migration InitialCreate
+Update-Database
+--
+Add-Migration WorkItemNextTime
+Script-Migration InitialCreate
