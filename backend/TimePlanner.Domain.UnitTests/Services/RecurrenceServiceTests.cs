abc17b2e26c5c0a70d6a47ef39b3d342ae3a90df
Aleksey Senichev <aleks.senichev@gmail.com>
2022-03-31 16:15:48 +0300

    Thirty first date tests

--- backend/TimePlanner.Domain.UnitTests/Services/RecurrenceServiceTests.cs
+++ backend/TimePlanner.Domain.UnitTests/Services/RecurrenceServiceTests.cs
@@ -13,6 +13,13 @@ namespace TimePlanner.Domain.UnitTests.Services
     static string TimeString(DateTime time) => time.ToString(TimeFormat, CultureInfo.InvariantCulture);
     static DateTime Time(string str) => DateTime.ParseExact(str, TimeFormat, CultureInfo.InvariantCulture);
 
+    private void RunTest(string relateveTo, string cronExpression, string expected)
+    {
+      DateTime? next = recurrenceService.CalculateNextTime(cronExpression, Time(relateveTo));
+      Assert.IsTrue(next.HasValue);
+      Assert.AreEqual(expected, TimeString(next.Value));
+    }
+
     [SetUp]
     public void SetUp()
     {
@@ -20,18 +27,31 @@ namespace TimePlanner.Domain.UnitTests.Services
     }
 
     [TestCase("every sunday", "31/03/2022 14:30", "0 0 * * 0", "03/04/2022 00:00")]
+    [TestCase("every sunday", "31/03/2022 14:30", "0 0 ? * 0", "03/04/2022 00:00")]
     [TestCase("every saturday", "31/03/2022 01:05", "0 0 * * 6", "02/04/2022 00:00")]
+    [TestCase("every saturday", "31/03/2022 01:05", "0 0 ? * 6", "02/04/2022 00:00")]
     [TestCase("every monday", "31/03/2022 01:05", "0 0 * * 1", "04/04/2022 00:00")]
-    public void TestDays(string testName, string baseTime, string cron, string expected)
+    [TestCase("every monday", "31/03/2022 01:05", "0 0 * ? 1", "04/04/2022 00:00")]
+    public void TestWeekDays(string testName, string relativeTo, string cronExpression, string expected)
     {
-      DateTime? next = recurrenceService.CalculateNextTime(cron, Time(baseTime));
-      Assert.IsTrue(next.HasValue);
-      Assert.AreEqual(expected, TimeString(next.Value));
+      RunTest(relativeTo, cronExpression, expected);
     }
 
-    [Test]
-    public void TestMonths()
+    [TestCase("", "31/03/2016 00:00", "0 0 31W * *", "31/03/2016 00:00")]
+    [TestCase("", "31/03/2016 00:00", "0 0 31W * ?", "31/03/2016 00:00")]
+    [TestCase("", "01/01/2016 00:00", "0 0 31W * *", "29/01/2016 00:00")]
+    [TestCase("", "01/01/2016 00:00", "0 0 31W * ?", "29/01/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 31W * *", "31/03/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 31W * ?", "31/03/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 30W * *", "30/03/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 30W * ?", "30/03/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 29W * *", "29/02/2016 00:00")]
+    [TestCase("", "01/02/2016 00:00", "0 0 29W * ?", "29/02/2016 00:00")]
+    [TestCase("", "01/02/2017 00:00", "0 0 29W * *", "29/03/2017 00:00")]
+    [TestCase("", "01/02/2017 00:00", "0 0 29W * ?", "29/03/2017 00:00")]
+    public void TestEndOfMonths(string testName, string relativeTo, string cronExpression, string expected)
     {
+      RunTest(relativeTo, cronExpression, expected);
     }
 
     [Test]
